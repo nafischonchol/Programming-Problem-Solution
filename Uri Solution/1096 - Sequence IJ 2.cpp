bea5291b11b88a72436b9cd2f2c8cde7d859a9de
nafischonchol <nafisislam161@gmail.com>
2020-02-14 23:09:25 +0600

    1096 - Sequence IJ 2

---
+++ Uri Solution/1096 - Sequence IJ 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i=1,j=7,x;
+    while(i<=9)
+    {
+        for(j=7;j>=5;j--)
+            printf("I=%d J=%d\n",i,j);
+        i=i+2;
+    }
+    return 0;
+}
