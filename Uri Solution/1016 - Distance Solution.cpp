bf720f1cd5c278caffc56584be6b41b0ff9408a2
nafischonchol <nafisislam161@gmail.com>
2020-02-14 22:17:36 +0600

    1016 - Distance

---
+++ Uri Solution/1016 - Distance Solution.cpp
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+       Scanner sc =new Scanner(System.in);
+       int d=sc.nextInt();
+       int m=2*d;
+       System.out.println(m+" minutos");
+    }
+}
