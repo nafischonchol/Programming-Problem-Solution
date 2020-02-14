801596096944ad65b7972dcb35df88145f2b5d7a
nafischonchol <nafisislam161@gmail.com>
2020-02-14 22:18:23 +0600

    1017 - Fuel Spent

---
+++ Uri Solution/1017 - Fuel Spent.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+ public static void main(String[] args) {
+ Scanner in=new Scanner (System.in);
+ int c=in.nextInt();
+ int k=in.nextInt();
+ double a=(double)(k*c)/12;
+ System.out.printf("%.3f\n",a);
+ }
+}   
+  
