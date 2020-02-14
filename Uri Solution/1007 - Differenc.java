b495cc56a6e88eafed03fe8c164f6d85bb3736a9
nafischonchol <nafisislam161@gmail.com>
2020-02-14 22:08:48 +0600

    1007 - Difference

---
+++ Uri Solution/1007 - Differenc.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+
+  public static void main(String[] args) {
+    Scanner in= new Scanner(System.in);
+    int A=in.nextInt();
+    int B=in.nextInt();
+    int C=in.nextInt();
+    int D=in.nextInt();
+    int ans=(A*B)-(C*D);
+    System.out.println("DIFERENCA = "+ans);
+    
+  }
+            
+}
